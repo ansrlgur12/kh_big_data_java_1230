23463415eb61c09cad74c1f0703ee939b97a5494
ansrlgur12 <ansrlgur12@naver.com>
2023-01-05 14:31:06 +0900

    1월5일 작업분

---
+++ Java_Big_Data_1230/src/복습_주사위게임/RandomGame.java
@@ -0,0 +1,22 @@
+package 복습_주사위게임;
+//주사위 2개 던져서 합 12이면 탈출하는 게임
+
+public class RandomGame {
+    public static void main(String[] args) {
+        int rand1, rand2;
+        int cnt = 0;
+        while(true) {
+            cnt++; //몇번만에 탈출했는지를 나타낼 변수
+            rand1 = (int) ((Math.random()*6) + 1); // Math.random . 랜덤함수는 실수형(0.1)으로 값이 생성되므로 형변환이 필요
+            rand2 = (int) ((Math.random()*6) + 1);
+            if ((rand1 + rand2) == 12){
+                System.out.printf("무인도를 %d번 만에 탈출 합니다.", cnt);
+                break;
+            }else {
+                System.out.println("주사위의 합이 " + (rand1 + rand2) + "입니다.");
+            }
+
+        }
+    }
+
+}
