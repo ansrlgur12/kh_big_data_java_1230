6e6301c76ea9cc6f946f2cb85ceb56de8003a335
ansrlgur12 <ansrlgur12@naver.com>
2023-01-20 12:45:10 +0900

    01.20 자동차 만들기

--- Java_Big_Data_1230/src/_0120_자동차만들기/Car.java
+++ Java_Big_Data_1230/src/_0120_자동차만들기/Car.java
@@ -16,20 +16,17 @@ public class Car {
     }
     void time(int area) {
         double time = (double)areaKm[area] / speed;
-        System.out.printf("%.2f시간", time);
+        System.out.printf("%.2f시간이 걸립니다", time);
     }
     public void setGasStation(int area) {
-
         int needOil = areaKm[area] / fuel;
         int countOil = needOil / tankSize;
-        System.out.println("주유소에 들려야할 횟수는 : " + (countOil+1) + " 입니다.");
+        System.out.println(name + "차량이 주유소에 들려야할 횟수는 " + (countOil+1) + "번 입니다.");
     }
     public void setAmount(int area) {
-
         int totalAmount = (areaKm[area] / fuel) * 2000;
-        System.out.println("들어갈 총 비용은 " + totalAmount + " 입니다.");
+        System.out.println("들어갈 총 비용은 " + totalAmount + "원 입니다.");
     }
-
 }
 class SportsCar extends Car {
     boolean isTurbo;
@@ -41,15 +38,14 @@ class SportsCar extends Car {
         tankSize = 30;
         seat = 2;
         fuel = 8;
+        System.out.println(name + " 를 탑니다.");
     }
     void turboOnOff() {
         System.out.print("보조기능을 켜시겠습니까? (y/n) : ");
         String onOff = sc.next();
         if (onOff.equalsIgnoreCase("Y")) speed *= 1.2;
     }
-
 }
-
 class Sedan extends Car {
     boolean isTrunk;
     Sedan(String name) {
@@ -60,13 +56,13 @@ class Sedan extends Car {
         tankSize = 45;
         seat = 4;
         fuel =12;
+        System.out.println(name + " 를 탑니다.");
     }
     void turboOnOff() {
         System.out.print("보조기능을 켜시겠습니까? (y/n) : ");
         String onOff = sc.next();
         if (onOff.equalsIgnoreCase("Y")) seat = 5;
     }
-
 }
 class Bus extends Car {
     boolean isTank;
@@ -78,17 +74,11 @@ class Bus extends Car {
         tankSize = 100;
         seat = 20;
         fuel = 5;
+        System.out.println(name + " 를 탑니다.");
     }
     void turboOnOff() {
         System.out.print("보조기능을 켜시겠습니까? (y/n) : ");
         String onOff = sc.next();
         if (onOff.equalsIgnoreCase("Y")) tankSize = 130;
-        else tankSize = 100;
     }
-
-
-
-
-
-
 }
--- Java_Big_Data_1230/src/_0120_자동차만들기/Main.java
+++ Java_Big_Data_1230/src/_0120_자동차만들기/Main.java
@@ -5,73 +5,75 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-            Bus bus = new Bus("고속버스");
-            SportsCar sportsCar = new SportsCar("포르쉐");
-            Sedan sedan = new Sedan("아반떼");
-
-
             Scanner sc = new Scanner(System.in);
             System.out.print("이동지역을 선택하세요 [1] 부산 [2] 대전 [3] 강릉 [4] 광주 : ");
             int area = sc.nextInt();
-            System.out.print("이동할 승객 수를 입력하세요 : ");
-            int passenger = sc.nextInt();
-
-            if (passenger > 5) {
-                System.out.println("버스밖에 못탑니다");
-                bus.turboOnOff();
-                bus.setAmount(area);
-                bus.setGasStation(area);
-                bus.time(area);
-            }
-            else if (passenger > 2) {
-                System.out.print("이동 차량을 선택하세요 [1] 승용차 [2] 버스 : ");
-                int v = sc.nextInt();
-                if (v == 1) {
-                    sedan.turboOnOff();
-                    sedan.setAmount(area);
-                    sedan.setGasStation(area);
-                    sedan.time(area);
-                    }
-                else{
+            if (area < 1 || area > 4) {
+                    System.out.print("잘못입력하였습니다");
+                    return;
+                }
+            while(true) {
+                System.out.print("이동할 승객 수를 입력하세요 : ");
+                int passenger = sc.nextInt();
+                if (passenger > 20) {
+                    System.out.println("최대 20명의 승객까지 가능합니다.");
+                }
+                else if (passenger > 5) {
+                    System.out.println(passenger + "명은 버스밖에 못탑니다");
+                    Bus bus = new Bus("고속버스");
                     bus.turboOnOff();
                     bus.setAmount(area);
                     bus.setGasStation(area);
                     bus.time(area);
-                }
-            }
-            else {
-                System.out.print("이동 차량을 선택하세요 [1] 스포츠카 [2] 승용차 [3] 버스 : ");
-                int v = sc.nextInt();
-                switch (v) {
-                    case 1 : {
-                        sportsCar.turboOnOff();
-                        sportsCar.setAmount(area);
-                        sportsCar.setGasStation(area);
-                        sportsCar.time(area);
-                        break;
-                    }
-                    case 2 : {
+                    break;
+                } else if (passenger > 2) {
+                    System.out.print("이동 차량을 선택하세요 [1] 승용차 [2] 버스 : ");
+                    int v = sc.nextInt();
+                    if (v == 1) {
+                        Sedan sedan = new Sedan("아반떼");
                         sedan.turboOnOff();
                         sedan.setAmount(area);
                         sedan.setGasStation(area);
                         sedan.time(area);
-                        break;
-                    }
-                    case 3 : {
+                    } else {
+                        Bus bus = new Bus("고속버스");
                         bus.turboOnOff();
                         bus.setAmount(area);
                         bus.setGasStation(area);
                         bus.time(area);
-                        break;
                     }
-                    default :
+                    break;
+                } else {
+                    System.out.print("이동 차량을 선택하세요 [1] 스포츠카 [2] 승용차 [3] 버스 : ");
+                    int v = sc.nextInt();
+                    switch (v) {
+                        case 1: {
+                            SportsCar sportsCar = new SportsCar("포르쉐");
+                            sportsCar.turboOnOff();
+                            sportsCar.setAmount(area);
+                            sportsCar.setGasStation(area);
+                            sportsCar.time(area);
+                            break;
+                        }
+                        case 2: {
+                            Sedan sedan = new Sedan("아반떼");
+                            sedan.turboOnOff();
+                            sedan.setAmount(area);
+                            sedan.setGasStation(area);
+                            sedan.time(area);
+                            break;
+                        }
+                        case 3: {
+                            Bus bus = new Bus("고속버스");
+                            bus.turboOnOff();
+                            bus.setAmount(area);
+                            bus.setGasStation(area);
+                            bus.time(area);
+                            break;
+                        }
+                        default:
+                    } break;
                 }
             }
-
-
-
-
-
-
     }
 }
