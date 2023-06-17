49de162d106c52be85cd7cefa5918701b1649f2d
KIHYUKMOON <121914919+ansrlgur12@users.noreply.github.com>
2023-06-17 22:41:24 +0900

    ----

--- Java_Big_Data_1230/src/_0605_인터페이스복습2/Audio.java
+++ Java_Big_Data_1230/src/_0605_인터페이스복습2/Audio.java
@@ -21,7 +21,7 @@ public class Audio implements RemoteControl{
         } else {
             this.volume = volume;
         }
-        System.out.println("현재 Audio 볼륨 : " + this.volume);
+        System.out.println("현재 볼륨 : " + this.volume);
     }
 
 
